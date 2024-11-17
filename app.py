02b1488783679847556f6b7bff87d7d651146c70
MiguelAngel861 <miguelangelscott861@gmail.com>
2024-11-17 09:35:41 -0600

    Cosas no muy importantes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import urllib.parse
 
 app = Flask(__name__)
 
-app.secret_key = '3lvKQGLL84O2599qeO21KFqbqYtSQfKb'
+app.secret_key = '3lvKQGLL84O2599qeO21KFqbqYtSQfKb' #Sin esto no funciona, por favor no tocar.
 
 CLIENT_ID = 'c9f3386b76074211a1a2e478661ea36c'
 CLIENT_SECRET = 'd11a3cf5187c41188ab53ebce5ffc8d3'
