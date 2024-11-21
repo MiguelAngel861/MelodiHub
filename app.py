3032426750094229f6d2c0a9236fa83de5d15f87
MiguelAngel861 <miguelangelscott861@gmail.com>
2024-11-20 22:16:50 -0600

    dios nos ayudo

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
-
 from flask import Flask, redirect, url_for, request, session, render_template
 import requests
 import urllib.parse
 
-
 app = Flask(__name__)
 
-app.secret_key = '3lvKQGLL84O2599qeO21KFqbqYtSQfKb' #Sin esto no funciona, por favor no tocar.
+app.secret_key = '3lvKQGLL84O2599qeO21KFqbqYtSQfKb'  # Sin esto no funciona, por favor no tocar.
 
 CLIENT_ID = 'c9f3386b76074211a1a2e478661ea36c'
 CLIENT_SECRET = 'd11a3cf5187c41188ab53ebce5ffc8d3'
 REDIRECT_URI = 'http://127.0.0.1:5000/callback'
 
-#constantes para las rutas
+# Constantes para las rutas
 AUTHN_URL = 'https://accounts.spotify.com/authorize'
-TOKEN_URL = 'https://accounts.sportify.com/api/token'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
 API_BASE_URL = 'https://api.spotify.com/v1/'
-SCOPE = 'user-library-read'
+SCOPE = 'user-library-read, user-read-recently-played'
 
 @app.route('/')
 def index():
-    return render_template ("login.html")
+    return render_template("login.html")
 
 @app.route('/login')
 def login():
     # Redireccionar al usuario a la página de autorización de Spotify
     auth_url = f"https://accounts.spotify.com/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={SCOPE}"
     return redirect(auth_url)
-
 
 @app.route('/callback')
 def callback():
@@ -55,9 +52,9 @@ def profile():
     user_info = response.json()
     return f"Bienvenido, {user_info['display_name']}!"
 
-@app.route("/Historial")
-def recently_played():
-    # Obtener las últimas 50 canciones escuchadas por el usuario
+@app.route('/historial')
+def Historial():
+    #canciones escuchadas la persiona
     headers = {"Authorization": f"Bearer {session['access_token']}"}
     params = {"limit": 50}
     response = requests.get("https://api.spotify.com/v1/me/player/recently-played", headers=headers, params=params)
@@ -74,6 +71,6 @@ def recently_played():
         return render_template("historial.html", tracks=tracks)
     else:
         return f"Error: {response.status_code} - {response.text}"
-    
-if __name__ == '_main_':
+
+if __name__ == '__main__':
     app.run(debug=True)
